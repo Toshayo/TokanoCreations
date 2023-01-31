3fe9d7e6eb3999521e4f8d8ea09b3c2a12cd28ca
Toshayo <abc126cba.abc126cba@yandex.ru>
2023-01-31 19:53:03 +0100

    Updated version

--- src/main/java/net/toshayo/idearunoken/IdearuNoKen.java
+++ src/main/java/net/toshayo/idearunoken/IdearuNoKen.java
@@ -29,7 +29,7 @@ import java.util.List;
 public class IdearuNoKen {
 	public static final String MOD_ID = "idearunoken";
 	public static final String NAME = "IdearuNoKen";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.1.0";
 
 	public static final Item SWORD_SHAPE = new SwordShapeItem();
 
