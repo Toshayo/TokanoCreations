859ef2a46fbc4b532dc395c2a03b3f386f58c610
Toshayo <abc126cba.abc126cba@yandex.ru>
2023-01-31 20:28:42 +0100

    Updated version

--- src/main/java/net/toshayo/tokanocreations/TokanoCreationsMod.java
+++ src/main/java/net/toshayo/tokanocreations/TokanoCreationsMod.java
@@ -29,7 +29,7 @@ import java.util.List;
 public class TokanoCreationsMod {
 	public static final String MOD_ID = "tokanocreations";
 	public static final String NAME = "Tokano Creations Mod";
-	public static final String VERSION = "1.1.0";
+	public static final String VERSION = "1.2.0";
 
 	public static final Item SWORD_SHAPE = new SwordShapeItem();
 
